4ae80b93b2907698658048742870d1c5b3d4bede
Andre Lare <aalarin@vtb.ru>
2025-09-18 14:50:38 +0300

    Добавлены методики расчета суммы, разности, произведения и частного в метод main.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int number = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+
+
+        int resulta = number+number2;
+        int resultb = number-number2;
+        int resultс = number*number2;
+        double resultd = (double) number/number2;
+
+
+        System.out.println("Сумма первого и второго числа = " + resulta);
+        System.out.println("Разность первого и второго числа = " + resultb);
+        System.out.println("Произведение первого и второго числа = " + resultс);
+        System.out.println("Частное первого и второго числа = " + resultd);
+
+
     }
 }
