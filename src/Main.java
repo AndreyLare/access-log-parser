b0df8673932d82be7c6b67ac8ea9766fc550c6a8
Andre Lare <aalarin@vtb.ru>
2025-09-18 14:59:43 +0300

    Изменил вывод сум\разность\произв\частн. +рефрактор наименования переменной number в number1

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Введите первое число:");
-        int number = new Scanner(System.in).nextInt();
+        int number1 = new Scanner(System.in).nextInt();
         System.out.println("Введите второе число:");
         int number2 = new Scanner(System.in).nextInt();
 
 
-        int resulta = number+number2;
-        int resultb = number-number2;
-        int resultс = number*number2;
-        double resultd = (double) number/number2;
+        int resulta = number1 + number2;
+        int resultb = number1 - number2;
+        int resultс = number1 * number2;
+        double resultd = (double) number1 /number2;
 
 
-        System.out.println("Сумма первого и второго числа = " + resulta);
-        System.out.println("Разность первого и второго числа = " + resultb);
-        System.out.println("Произведение первого и второго числа = " + resultс);
-        System.out.println("Частное первого и второго числа = " + resultd);
+        System.out.println("Сумма = " + resulta);
+        System.out.println("Разность = " + resultb);
+        System.out.println("Произведение = " + resultс);
+        System.out.println("Частное = " + resultd);
 
 
     }
