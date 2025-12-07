aa9cec7918ba1c31be8271cd2fc940b4b54741db
Andre Lare <aalarin@vtb.ru>
2025-12-07 18:54:40 +0300

    Изменил вывод сум\разность\произв\частн. +рефрактор наименования переменной number в number1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        int result = 0;
+        while (true){
+            System.out.println("Введите путь к файлу (или '0' для выхода): ");
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (path.equalsIgnoreCase("0")) {
+                break;
+            }
+            if (!fileExist) {
+                System.out.println("файл не существует или указанный путь является путём к папке, а не к файлу");
+                continue;
+            }
+            if (!isDirectory && fileExist) {
+                System.out.println("Путь указан верно");
+                result ++;
+                System.out.println("Это файл номер " + result);
+            }
+        }
     }
 }
